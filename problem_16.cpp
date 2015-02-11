284fe9135cb6e2db64adf1b7c14f4bcb0b863f4c
Zach McNeilly <zach.mcneilly@gmail.com>
2015-02-11 12:27:11 -0500

    Added helpful comments to problem 16

--- problem_16.cpp
+++ problem_16.cpp
@@ -2,9 +2,15 @@
 #include <gmpxx.h>
 
 int main() {
-  mpz_class large_int;
+  mpz_class large_int; // C++ wrapper around GMP
+  /*
+   * Some useful functions are used here
+   * mpz_class.get_mpz_t() returns an mpz_t instance of the mpz_class object
+   * This function performs a power operation on the mpz_t object passed to it
+   */
   mpz_pow_ui(large_int.get_mpz_t(), mpz_class("2").get_mpz_t(), 1000);
   int total = 0;
+  // for (var : iterable object) is python equivalent for var in iterable:
   for (char& c : large_int.get_str()) {
     total += (int) c-'0';
   }
